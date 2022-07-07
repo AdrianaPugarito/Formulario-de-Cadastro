23cf353a933d707ea9f28894b5015c6470abdc0b
Adriana Pugarito <85944220+AdrianaPugarito@users.noreply.github.com>
2022-07-07 19:40:55 +0000

    Mostrando as mensagens de erro

--- JS/validacao.js
+++ JS/validacao.js
@@ -1,4 +1,4 @@
-/*const dataNascimento = document.querySelector('#nascimento');
+x/*const dataNascimento = document.querySelector('#nascimento');
 
 dataNascimento.addEventListener('blur', (evento) => {
   validaDataNascimento(evento.target);
@@ -10,9 +10,35 @@ export function valida(input) {
   if(validadores[tipoDeInput]) {
     validadores[tipoDeInput](input);
   }
+
+  if(input.validity.valid) {
+    input.parentElement.classList.remove('input-container--invalido')
+  } else {
+    input.parenteElement.classList.add('input-container--invalido')
+  }
+}
+
+const mensagensDeErro = {
+  nome:{
+    valueMissing: 'O campo nome não pode estar vazio'
+  },
+  email:{
+    valueMissing: 'O campo de email não pode estar vazio',
+    typeMismatch: 'O email digitado não e valido'
+  },
+  senha:{
+    valueMissing: 'O campo de senha não pode estar vazio',
+    patternMismatch: 'A senha deve conter: entre 6 a 12 caracteres, pelo menos uma letra maiúscula, um número e não símbolos'
+  },
+  dataNascimento:{
+    valueMissing: 'O campo data de nascimento não pode estar vazio',
+    customError: 'Você dever ser maoir que 18 anos para se cadastrar'
+  }
+  
 }
 
 const validadores = {
+  
   dataNascimento : input => validaDataNascimento(input)
 }
 
